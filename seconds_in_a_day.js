78be87f439ef5a7f10ec09f09c6d9e2934767617
Bozsák Barnabás <b3bozbar@vasvari.org>
2026-09-07 09:40:49 +0200

    New file

---
+++ seconds_in_a_day.js
@@ -0,0 +1,4 @@
+import inout from "./input.js";
+let currentHours = await input("Please enter the curent hour (1-24): ");
+let currentMinutes = await input("Please enter the curent minutes (0-59): ");
+let currentSeconds = await input("Please enter the curent seconds (0-59): ");
