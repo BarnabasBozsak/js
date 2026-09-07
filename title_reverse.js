a1fe4b5ad9822814b56c9e5cc3ef0a53a4e346bc
Bozsák Barnabás <b3bozbar@vasvari.org>
2026-09-07 12:30:36 +0200

    Modositva

--- title_reverse.js
+++ title_reverse.js
@@ -4,7 +4,7 @@ let newTitle = "";
 for (let i = title.length -1; i >= 0; i--)
 {
     if (i > 0) {
-        newTitle += title.charAt(i);
+        newTitle += title.charAt(i).toLowerCase();
     }
     else{
         newTitle += title.charAt(i).toUpperCase();
