26a3c895645b5146296cfb5c91542fa853f64e11
barniOtthon <b3bozbar@vasvari.org>
2026-09-09 20:23:31 +0200

    Ne file

--- bevezeto.js
+++
@@ -1,6 +0,0 @@
-import input from "./input.js";
-let a = await input("Kérek egy számot!");
-console.log("A szám:" + a);
-let b = await input("Kérek egy másik számot!");
-console.log("A másik szám:" + b);
-console.log(" A két szám összege:" + (parseInt(a) + parseInt(b)));
--- seconds_in_a_day.js
+++
@@ -1,9 +0,0 @@
-import input from "./input.js";
-let currentHours = await input("Please enter the curent hour (1-24): ");
-let currentMinutes = await input("Please enter the curent minutes (0-59): ");
-let currentSeconds = await input("Please enter the curent seconds (0-59): ");
-let hoursLeft = 24 - currentHours;
-let minutesLeft = 60 - currentMinutes;
-let secondsLeft = 60 - currentSeconds;
-let totalSecondsLeft = (hoursLeft * 3600) + (minutesLeft * 60) + secondsLeft;
-console.log(`There are ${totalSecondsLeft} seconds left in the day.`);
