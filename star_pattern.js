c8036841e13818ae94a7260d4dc831cdaa8d00ee
b3bozbar <b3bozbar@vasvari.org>
2026-09-14 11:54:12 +0200

    Square shape

---
+++ star_pattern.js
@@ -0,0 +1,10 @@
+function printSquare() {
+  for (let i = 0; i <= 4; i++) {
+    let row = "";
+    for (let j = 0; j <= 5; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
+printSquare();
