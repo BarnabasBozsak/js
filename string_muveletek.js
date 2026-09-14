d0cd56c01d57263d96dd28723b7f1ff8da8a3cd0
b3bozbar <b3bozbar@vasvari.org>
2026-09-14 09:25:12 +0200

    1-4. feladat

--- string_muveletek.js
+++ string_muveletek.js
@@ -6,3 +6,18 @@ function elsoOt(text)
     return text.substring(0,5);
 }
 console.log(elsoOt(text));
+function haromtolNyolcig(text)
+{
+    return text.substring(3,8);
+}
+console.log(haromtolNyolcig(text));
+function ottolVegig(text)
+{
+    return text.substring(5);
+}
+console.log(ottolVegig(text));
+function ottolHatKarakter(text)
+{
+    return text.substr(5,6);
+}
+console.log(ottolHatKarakter(text));
