a29552c0d8187f354d002bf108e69d31eb742864
b3bozbar <b3bozbar@vasvari.org>
2026-09-14 09:27:45 +0200

    5-6. feladat

--- string_muveletek.js
+++ string_muveletek.js
@@ -1,23 +1,34 @@
 const { captureOwnerStack } = require("react");
 
-let text = "Ez egy példa szöveg, amelyet a JavaScript string műveletek bemutatására használok.";
-function elsoOt(text)
-{
-    return text.substring(0,5);
+let text =
+  "Ez egy példa szöveg, amelyet a JavaScript string műveletek bemutatására használok.";
+function elsoOt(text) {
+  return text.substring(0, 5);
 }
 console.log(elsoOt(text));
-function haromtolNyolcig(text)
-{
-    return text.substring(3,8);
+function haromtolNyolcig(text) {
+  return text.substring(3, 8);
 }
 console.log(haromtolNyolcig(text));
-function ottolVegig(text)
-{
-    return text.substring(5);
+function ottolVegig(text) {
+  return text.substring(5);
 }
 console.log(ottolVegig(text));
-function ottolHatKarakter(text)
-{
-    return text.substr(5,6);
+function ottolHatKarakter(text) {
+  return text.substr(5, 6);
 }
 console.log(ottolHatKarakter(text));
+function nagybetus(text) {
+  return text.toUpperCase();
+}
+console.log(nagybetus(text));
+function mindenMasodikNagybetu(text) {
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    if (i % 2 === 0) {
+      result += text[i].toUpperCase();
+    }
+  }
+  return result;
+}
+console.log(mindenMasodikNagybetu(text));
