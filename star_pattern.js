728f5046f23fa26984707afc4f141cb5541c04db
b3bozbar <b3bozbar@vasvari.org>
2026-09-14 11:57:16 +0200

    Triangle and inverted triangle

--- star_pattern.js
+++ star_pattern.js
@@ -8,3 +8,23 @@ function printSquare() {
   }
 }
 printSquare();
+function printTriangle() {
+  for (let i = 0; i <= 4; i++) {
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
+printTriangle();
+function printInvertedTriangle() {
+  for (let i = 5; i >= 0; i--) {
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
+printInvertedTriangle();
